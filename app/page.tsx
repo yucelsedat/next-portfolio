463f232ea72a725db9373fd820e3d4936f91b4ad
yucelsedat <yucelsedatt@gmail.com>
2024-12-19 15:07:58 +0300

    about section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import About from "./components/About"
 import Intro from "./components/Intro"
+import SectionDivider from "./components/Section-divider"
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4 max-w-[50rem] !text-center mx-auto mb-28 sm:mb-0">
       <Intro />
+      <SectionDivider />
+      <About />
     </main>
   )
 }
