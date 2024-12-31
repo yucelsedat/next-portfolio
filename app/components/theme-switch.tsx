cf467d405afa0fe22f168e3d6a307a29ac80dbcd
yucelsedat <yucelsedatt@gmail.com>
2024-12-31 17:42:49 +0300

    dark mode button style fixed

--- app/components/theme-switch.tsx
+++ app/components/theme-switch.tsx
@@ -10,7 +10,7 @@ export default function ThemeSwitch() {
   const  { theme, toggleTheme } = useTheme()
 
   return (
-    <button className='fixed right-6 bottom-6 bg-white bg-opacity-80 backdrop-blur-[.5rem] p-3 rounded-full border border-white border-opacity-40 shadow-2xl hover:scale-[1.15] active:scale-105 transition-all'
+    <button className='fixed right-6 bottom-6 bg-white bg-opacity-80 dark:bg-gray-950 backdrop-blur-[.5rem] p-3 rounded-full border border-white border-opacity-40 dark:border-none shadow-2xl hover:scale-[1.15] active:scale-105 transition-all'
       onClick={toggleTheme}
     >
       {theme === 'light' ? <BsSun /> : <BsMoon />}
